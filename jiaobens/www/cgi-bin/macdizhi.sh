2a5ea70d15790819a133bbe527294b4999286852
changlz <changlizhi@aliyun.com>
2018-01-02 15:40:44 +0800

    ifcongif canshu

--- jiaobens/www/cgi-bin/macdizhi.sh
+++ jiaobens/www/cgi-bin/macdizhi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mac1=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 1`
-mac2=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 2`
-mac3=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 3`
-mac4=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 4`
-mac5=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 5`
-mac6=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 6`
+mac1=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 1`
+mac2=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 2`
+mac3=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 3`
+mac4=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 4`
+mac5=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 5`
+mac6=`ifconfig|grep 'eth0'|grep -v '\.'|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 6`
 macall=$mac1$mac2$mac3$mac4$mac5$mac6
 echo $macall
 tmp=`echo $macall | sed 's/:/ /g'`
