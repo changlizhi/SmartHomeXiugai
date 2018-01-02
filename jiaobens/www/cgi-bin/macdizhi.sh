728db592d7320bd3801209b2da9c86c8f33b2dfb
root <root@ubuntu.(none)>
2018-01-02 02:20:53 -0500

    zengjia macdizhi.sh

---
+++ jiaobens/www/cgi-bin/macdizhi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mac1=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 1`
+mac2=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 2`
+mac3=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 3`
+mac4=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 4`
+mac5=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 5`
+mac6=`ifconfig|grep HWaddr|cut -d'r' -f 3|cut -d':' -f 6`
+macall=$mac1$mac2$mac3$mac4$mac5$mac6
+echo $macall
+tmp=`echo $macall | sed 's/:/ /g'`
+       echo -n '' > dec_mac
+        
+       for hex in $tmp
+       do
+         dec=`printf "%d" 0x$hex`
+         echo $dec >> dec_mac
+      done
+       
+      cat dec_mac | xargs | sed 's/ /:/g; s/:$//g'
+
